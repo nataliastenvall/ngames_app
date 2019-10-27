6510123a4a1f08fe8d1d2982ebbe1229d1c5d27e
Koroleva Natalia <s1900164@edu.bc.fi>
2019-10-27 12:03:35 +0200

    combineReducers user and page created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">My data</h1>
+        </header>
+        <p className="App-intro">Here will be my data</p>
+        <p>My name is: {}</p>
+      </div>
+    );
+  }
 }
-
-export default App;
+const mapStateToProps = store => {
+  console.log(store);
+  return {
+    user: store.user
+  };
+};
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/user.js
@@ -0,0 +1,6 @@
+const initialState = {
+  name: "Anonym"
+};
+export function userReducer(state = initialState) {
+  return state;
+}
